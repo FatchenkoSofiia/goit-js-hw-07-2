78e5c3d5dce7b3316e8515ebd40fdefaef721e05
FatchenkoSofiia <fatcenkosona@gmail.com>
2023-12-08 20:25:00 +0200

    required

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", onLinkClick);
+
+function createGalleryMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+    alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
 console.log(galleryItems);
+
+function onLinkClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  const fullSizeItemLink = e.target.dataset.source;
+
+  const instance = basicLightbox.create(` <img src="${fullSizeItemLink}">`);
+
+  instance.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+
+      document.removeEventListener("keydown", e);
+    }
+  });
+}
